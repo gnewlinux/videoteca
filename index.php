ef5df72efd71cdba6b0eb72ce9cef54cdb7513a9
GnewLinux <gnewlinux@gmail.com>
2017-09-14 09:45:55 -0300

    Videoteca_Base

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Videoteca Webadmin</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+
+<div id="container">
+	<div id="header">
+	<ul>
+		<li><a href="#home">Home</a></li>
+		<li><a href="#news">Clientes</a></li>
+		<li><a href="#contact">Filmes</a></li>
+		<li style="float:right"><a class="active" href="#about">Sobre</a></li>
+	</ul>
+	</div>
+
+	<div id="content">
+		<div id=m"main">
+			<h2>Colored Table Header</h2>
+
+			<table>
+			<tr>
+			<th>Firstname</th>
+			<th>Lastname</th>
+			<th>Savings</th>
+			</tr>
+			<tr>
+			<td>Peter</td>
+			<td>Griffin</td>
+			<td>$100</td>
+			</tr>
+			<tr>
+			<td>Lois</td>
+			<td>Griffin</td>
+			<td>$150</td>
+			</tr>
+			<tr>
+			<td>Joe</td>
+			<td>Swanson</td>
+			<td>$300</td>
+			</tr>
+			<tr>
+			<td>Cleveland</td>
+			<td>Brown</td>
+			<td>$250</td>
+			</tr>
+			</table>
+		</div>
+	</div>
+
+	<div id="footer">
+		
+	</div>
+</div>
+
+
+</body>
+</html>
